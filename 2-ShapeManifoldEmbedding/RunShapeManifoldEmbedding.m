219c52f07c35e7dff328fe11ca5e784d28704d3e
Laura <l.cooper.5@warwick.ac.uk>
2025-12-01 16:51:45 +0000

    enforce must calculate at least 2 diffusion coordinates@

--- 2-ShapeManifoldEmbedding/RunShapeManifoldEmbedding.m
+++ 2-ShapeManifoldEmbedding/RunShapeManifoldEmbedding.m
@@ -4,9 +4,13 @@ out=SelectFolderEigen();
 dataFile = fullfile(out.folder, 'Bigcellarrayandindex.mat');
 global PATH;
 PATH=out.folder;
-if exist(dataFile, 'file')
+if exist(dataFile, 'file') && out.dims >=2
     data = load(dataFile);
     ShapeManifoldEmbedding_finalSJ(data.BigCellArray, out.folder, out.sparse, out.dims);
+elseif out.dims<2
+    display('-------')
+    display('Number of dimensions must be greater than 2');
+    display('-------')
 else 
     display('-------')
     display('The file "Bigcellarrayandindex.mat" does not exist in your Analysis folder.');
